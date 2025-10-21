53ad21a5bb7d1b9ddaa5e83da3ce1343b0f896b8
vikram-42-alla <allatrivikram@gmail.com>
2025-10-21 20:52:26 +0530

    commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/auth/signup", {
+      const response = await fetch("https://spring-server-0m1e.onrender.com/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
