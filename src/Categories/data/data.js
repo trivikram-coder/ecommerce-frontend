359972369c6313dddf6bdbdf5c31e99c067a9a5c
vikram-42-alla <allatrivikram@gmail.com>
2026-04-30 17:43:06 +0530

    fix: correct data mapping in getProducts function and add console log for fetched products

--- src/Categories/data/data.js
+++ src/Categories/data/data.js
@@ -3,8 +3,8 @@ import {apiUrl} from "../../service/api";
 export const getProducts = async () => {
   const response = await fetch(`${apiUrl}/products`);
   const data = await response.json();
-
-  return data.map(({ _id, ...rest }) => ({
+  console.log("Products ",data)
+  return data.products.map(({ _id, ...rest }) => ({
     productId: _id,
     ...rest
   }));
